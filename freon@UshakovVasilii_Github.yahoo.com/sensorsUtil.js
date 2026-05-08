375cd5df96e9728c7283fd04fc9a5072300fc917
Dominik Jarmulowicz <dominik.jarmulowicz@oversee.biz>
2026-05-09 00:25:17 +0200

    perf(sensors): single-pass parse with cached sensor groups
    Each getter (temp/gpu/disks/rpm/volt/power) used to walk the
    entire parsed JSON and re-run the family/key regexes. Per UI
    refresh that meant six full passes over the same data.
    
    Build all six groups in a single pass inside execute and have
    the getters return the cached arrays.

--- freon@UshakovVasilii_Github.yahoo.com/sensorsUtil.js
+++ freon@UshakovVasilii_Github.yahoo.com/sensorsUtil.js
@@ -2,6 +2,22 @@ import GLib from 'gi://GLib';
 
 import CommandLineUtil from './commandLineUtil.js';
 
+const TEMP_RE = /^temp\d+_input/;
+const FAN_RE = /^fan\d+_input/;
+const VOLT_RE = /^in\d+_input/;
+const POWER_RE = /^power\d+_average/;
+const GPU_CHIPSET_RE = /(radeon|amdgpu|nouveau)/;
+const DISK_CHIPSET_RE = /(drivetemp|nvme)/;
+
+const EMPTY_GROUPS = Object.freeze({
+    temp: [],
+    gpu: [],
+    disks: [],
+    rpm: [],
+    volt: [],
+    power: [],
+});
+
 export default class SensorsUtil extends CommandLineUtil {
 
     constructor() {
@@ -9,12 +25,12 @@ export default class SensorsUtil extends CommandLineUtil {
         let path = GLib.find_program_in_path('sensors');
         // -A: Do not show adapter -j: JSON output
         this._argv = path ? [path, '-A', '-j'] : null;
+        this._groups = EMPTY_GROUPS;
     }
 
-    // Avoid parsing the data more than once.
     execute(callback) {
         super.execute(() => {
-            let data = [];
+            let data = null;
             try {
                 data = JSON.parse(this._output.join(''));
             } catch (e) {
@@ -23,83 +39,72 @@ export default class SensorsUtil extends CommandLineUtil {
                     // https://github.com/UshakovVasilii/gnome-shell-extension-freon/issues/114#issuecomment-491613545
                     let lineRemoved = this._output.filter(l => l.trim() !== ',').join('\n');
                     let errorRemoved = lineRemoved.replace(/ERROR.*Can't read/, "");
-                        errorRemoved = errorRemoved.replace(/ERROR.*I\/O error/, "");
-                        errorRemoved = errorRemoved.replace(/NaN/, "0");
-                        data = JSON.parse(errorRemoved);
-                } catch (e) {
-                    logError(e);
-                    return [];
+                    errorRemoved = errorRemoved.replace(/ERROR.*I\/O error/, "");
+                    errorRemoved = errorRemoved.replace(/NaN/, "0");
+                    data = JSON.parse(errorRemoved);
+                } catch (e2) {
+                    logError(e2);
+                    this._groups = EMPTY_GROUPS;
+                    callback();
+                    return;
                 }
             }
-            this._data = data;
+            this._groups = this._buildGroups(data);
             callback();
         });
     }
 
-    get temp() {
-        return this._parseSensorsOutput(/^temp\d+_input/, 'temp', 'generic');
-    }
+    _buildGroups(data) {
+        const groups = { temp: [], gpu: [], disks: [], rpm: [], volt: [], power: [] };
 
-    get gpu() {
-        return this._parseSensorsOutput(/^temp\d+_input/, 'temp', 'gpu');
-    }
-
-    get disks() {
-        return this._parseSensorsOutput(/^temp\d+_input/, 'temp', 'disk');
-    }
-
-    get rpm() {
-        return this._parseSensorsOutput(/^fan\d+_input/, 'rpm', 'generic');
-    }
-
-    get volt() {
-        return this._parseSensorsOutput(/^in\d+_input/, 'volt', 'generic');
-    }
-
-    get power() {
-        return this._parseSensorsOutput(/^power\d+_average/, 'power', 'gpu');
-    }
-
-  _parseSensorsOutput(sensorFilter, sensorType, sensorFamily) {
-        if(!this._data)
-            return [];
-
-        const data = this._data;
-
-        let sensors = [];
-        for (var chipset in data) {
-            let tempType = (sensorType === 'temp')
-            let powerType = (sensorType === 'power')
-
-            let gpuFilter = /(radeon|amdgpu|nouveau)/;
-            let gpuFamily = (sensorFamily === 'gpu')
-
-            if (!data.hasOwnProperty(chipset) || (gpuFamily != gpuFilter.test(chipset) && (tempType || powerType)))
+        for (const chipset in data) {
+            if (!data.hasOwnProperty(chipset))
                 continue;
 
-            let diskFilter = /(drivetemp|nvme)/;
-            let diskFamily = (sensorFamily === 'disk')
-            if (!data.hasOwnProperty(chipset) || (diskFamily != diskFilter.test(chipset) && tempType))
-                continue;
+            const isGpu = GPU_CHIPSET_RE.test(chipset);
+            const isDisk = DISK_CHIPSET_RE.test(chipset);
+            const chipsetSensors = data[chipset];
 
-            let chipsetSensors = data[chipset]
-            for (var sensor in chipsetSensors) {
+            for (const sensor in chipsetSensors) {
                 if (!chipsetSensors.hasOwnProperty(sensor))
                     continue;
 
-                let fields = chipsetSensors[sensor];
-                for (var key in fields) {
-                    if (fields.hasOwnProperty(key) && sensorFilter.test(key)) {
-                        let feature = {
-                            label: sensor,
-                            [sensorType]: parseFloat(fields[key])
-                        };
-                        sensors.push(feature);
+                const fields = chipsetSensors[sensor];
+
+                for (const key in fields) {
+                    if (!fields.hasOwnProperty(key))
+                        continue;
+
+                    if (TEMP_RE.test(key)) {
+                        const entry = { label: sensor, temp: parseFloat(fields[key]) };
+                        if (isGpu) groups.gpu.push(entry);
+                        else if (isDisk) groups.disks.push(entry);
+                        else groups.temp.push(entry);
+                        break;
+                    }
+                    if (FAN_RE.test(key)) {
+                        groups.rpm.push({ label: sensor, rpm: parseFloat(fields[key]) });
+                        break;
+                    }
+                    if (VOLT_RE.test(key)) {
+                        groups.volt.push({ label: sensor, volt: parseFloat(fields[key]) });
+                        break;
+                    }
+                    if (POWER_RE.test(key) && isGpu) {
+                        groups.power.push({ label: sensor, power: parseFloat(fields[key]) });
                         break;
                     }
                 }
             }
         }
-        return sensors;
+
+        return groups;
     }
+
+    get temp()  { return this._groups.temp; }
+    get gpu()   { return this._groups.gpu; }
+    get disks() { return this._groups.disks; }
+    get rpm()   { return this._groups.rpm; }
+    get volt()  { return this._groups.volt; }
+    get power() { return this._groups.power; }
 };
